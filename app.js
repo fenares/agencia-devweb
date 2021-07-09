addd999e2641df08ef1f617031b82f3360be8541
Felipe Linares <felipelinares@uol.com.br>
2021-07-08 22:08:54 -0300

    Criando as páginas Home e Blog

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 //const http = require('http');
 //http.createServer(express).listen(3000, ( ) => console.log("Levantando um servidor"));
 
+//Esta página é executada pelo comando npm start, pois há um scrip inserido manualmente no package.json na linha 8
+
 const express = require('express');
 const app = express()
+app.listen(3000, () => console.log("Server running!"))
 
-app.get("/", (req, res) => { res.send("Welcome, padawan!")})
+app.use(express.static('public'))
+
+app.get("/", (req, res) => { res.sendFile(__dirname + "/views/home.html")})
+app.get("/blog.html", (req, res) => { res.sendFile(__dirname + "/views/blog.html")})
+app.get("/home.html", (req, res) => { res.redirect("/")})
 app.get("/maintenance", (req, res) => { res.sendFile(__dirname + "/views/maintenance.html")})
 
-app.listen(3000, () => console.log("Server running!"))
+
+
+
