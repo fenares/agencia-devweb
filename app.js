ec78252889c9904d38b1b681119fda1fa1effd67
Felipe Linares <felipelinares@uol.com.br>
2021-07-13 22:02:25 -0300

    Estilizando e Nodemon

--- app.js
+++ app.js
@@ -3,6 +3,8 @@
 
 //Esta página é executada pelo comando npm start, pois há um scrip inserido manualmente no package.json na linha 8
 
+// Para instalar o nodemon como dev => npm install nodemon -D. Para utilizar o nodemon no Windows, é preciso executar o comando "Set-ExecutionPolicy Unrestricted" no Power. E depois de alterar o scrip no Package.json, utilizar o "npm run app.js"
+
 const express = require('express');
 const app = express()
 app.listen(3000, () => console.log("Server running!"))
